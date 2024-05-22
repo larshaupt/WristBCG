f356c4195c67bfe5c615b7cad3d49b5b840dfa27
Lars Hauptmann <lhauptmann@sip-ml.inf.ethz.ch>
2024-05-22 16:55:28 +0200

    changed config file

--- config.py
+++ config.py
@@ -1,52 +1,62 @@
-data_dir_Apple = "/local/home/lhauptmann/thesis/data/AppleDataset"
-analysis_dir_Apple = '/local/home/lhauptmann/thesis/analysis/AppleDataset'
+from os.path import join
 
-data_dir_Apple_processed = "/local/home/lhauptmann/thesis/data/AppleDataset_0to6_50Hz"
-data_dir_Apple_processed_100hz = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz"
-data_dir_Apple_processed_100hz_wmetrics = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_metric_int"
-data_dir_Apple_processed_100hz_5w_4s = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_5w_4s"
-data_dir_Apple_processed_100hz_8w_4s = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_8w_4s"
-data_dir_Apple_processed_100hz_10w_4s = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_10w_4s"
-data_dir_Apple_processed_100hz_30w_4s = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_30w_4s"
-data_dir_Apple_processed_100hz_60w_4s = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_60w_4s"
-data_dir_Apple_processed_100hz_10w_2s = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_10w_2s"
-data_dir_Apple_processed_100hz_10w_8s = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_10w_8s"
-data_dir_Apple_processed_100hz_10w_1s = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_10w_1s"
-data_dir_Apple_processed_100hz_10w_10s = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_10w_10s"
-data_dir_Apple_processed_all = "/local/home/lhauptmann/thesis/data/AppleDataset_22to7_100Hz_all"
+# Change your base directory here
+base_dir = "/local/home/lhauptmann/thesis/"
 
-data_dir_M2Sleep = '/local/home/lhauptmann/thesis/data/USI Sleep/E4_Data'
-analysis_dir_M2Sleep = '/local/home/lhauptmann/thesis/analysis/M2Sleep'
+# Define all paths
+data_dir = join(base_dir, "data")
+analysis_dir = join(base_dir, "analysis")
+code_dir = join(base_dir, "CL-HAR")
 
-data_dir_M2Sleep_processed = "/local/home/lhauptmann/thesis/data/M2Sleep_processed_50Hz"
-data_dir_M2Sleep_processed_100Hz = '/local/home/lhauptmann/thesis/data/M2Sleep_processed_100Hz'
+data_dir_Apple = join(data_dir, "AppleDataset")
+analysis_dir_Apple = join(analysis_dir, "AppleDataset")
 
-data_dir_MaxDataset = "/local/home/lhauptmann/thesis/data/MaxDataset"
-data_dir_MaxDataset_v2 = "/local/home/lhauptmann/thesis/data/WristBCG"
-analysis_dir_MaxDataset = '/local/home/lhauptmann/thesis/analysis/MaxDataset'
+data_dir_Apple_processed = join(data_dir, "AppleDataset_0to6_50Hz")
+data_dir_Apple_processed_100hz = join(data_dir, "AppleDataset_22to7_100Hz")
+data_dir_Apple_processed_100hz_wmetrics = join(data_dir, "AppleDataset_22to7_100Hz_metric_int")
+data_dir_Apple_processed_100hz_5w_4s = join(data_dir, "AppleDataset_22to7_100Hz_5w_4s")
+data_dir_Apple_processed_100hz_8w_4s = join(data_dir, "AppleDataset_22to7_100Hz_8w_4s")
+data_dir_Apple_processed_100hz_10w_4s = join(data_dir, "AppleDataset_22to7_100Hz_10w_4s")
+data_dir_Apple_processed_100hz_30w_4s = join(data_dir, "AppleDataset_22to7_100Hz_30w_4s")
+data_dir_Apple_processed_100hz_60w_4s = join(data_dir, "AppleDataset_22to7_100Hz_60w_4s")
+data_dir_Apple_processed_100hz_10w_2s = join(data_dir, "AppleDataset_22to7_100Hz_10w_2s")
+data_dir_Apple_processed_100hz_10w_8s = join(data_dir, "AppleDataset_22to7_100Hz_10w_8s")
+data_dir_Apple_processed_100hz_10w_1s = join(data_dir, "AppleDataset_22to7_100Hz_10w_1s")
+data_dir_Apple_processed_100hz_10w_10s = join(data_dir, "AppleDataset_22to7_100Hz_10w_10s")
+data_dir_Apple_processed_all = join(data_dir, "AppleDataset_22to7_100Hz_all")
 
-data_dir_Max_processed = "/local/home/lhauptmann/thesis/data/MaxDataset_0to7_100Hz"
-data_dir_Max_processed_v2 = "/local/home/lhauptmann/thesis/data/MaxDataset_v2"
-data_dir_Max_processed_hrv = "/local/home/lhauptmann/thesis/data/MaxDataset_hrv"
+data_dir_M2Sleep = join(data_dir, "USI Sleep/E4_Data")
+analysis_dir_M2Sleep = join(analysis_dir, "M2Sleep")
 
-data_dir_Capture24 = "/local/home/lhauptmann/thesis/data/capture24"
+data_dir_M2Sleep_processed = join(data_dir, "M2Sleep_processed_50Hz")
+data_dir_M2Sleep_processed_100Hz = join(data_dir, "M2Sleep_processed_100Hz")
 
-data_dir_Capture24_processed = "/local/home/lhauptmann/thesis/data/capture24_processed_100Hz_sleep"
-data_dir_Capture24_processed_all = "/local/home/lhauptmann/thesis/data/capture24_processed_100Hz_all"
-data_dir_Capture24_processed_125Hz_8w = "/local/home/lhauptmann/thesis/data/capture24_processed_125Hz_8w_sleep"
+data_dir_MaxDataset = join(data_dir, "MaxDataset")
+data_dir_MaxDataset_v2 = join(data_dir, "WristBCG")
+analysis_dir_MaxDataset = join(analysis_dir, "MaxDataset")
 
-data_dir_Parkinson = "/local/home/lhauptmann/thesis/data/anonymized.h5"
-analysis_dir_Parkinson = '/local/home/lhauptmann/thesis/analysis/Parkinson'
+data_dir_Max_processed = join(data_dir, "MaxDataset_0to7_100Hz")
+data_dir_Max_processed_v2 = join(data_dir, "MaxDataset_v2")
+data_dir_Max_processed_hrv = join(data_dir, "MaxDataset_hrv")
 
-data_dir_Parkinson_processed = "/local/home/lhauptmann/thesis/data/Parkinson_22to7_100Hz"
-data_dir_Parkinson_processed_100Hz_wmetrics = "/local/home/lhauptmann/thesis/data/Parkinson_22to7_100Hz_wmetrics"
-data_dir_Parkinson_processed_100Hz = "/local/home/lhauptmann/thesis/data/Parkinson_22to7_100Hz"
+data_dir_Capture24 = join(data_dir, "capture24")
 
-data_dir_IEEE = "/local/home/lhauptmann/thesis/data/IEEE"
-data_dir_IEEE_processed = "/local/home/lhauptmann/thesis/data/IEEE_processed"
+data_dir_Capture24_processed = join(data_dir, "capture24_processed_100Hz_sleep")
+data_dir_Capture24_processed_all = join(data_dir, "capture24_processed_100Hz_all")
+data_dir_Capture24_processed_125Hz_8w = join(data_dir, "capture24_processed_125Hz_8w_sleep")
 
-ResNET_oxwearables_weights_path = "/local/home/lhauptmann/thesis/CL-HAR/models/mtl_best.mdl"
+data_dir_Parkinson = join(data_dir, "anonymized.h5")
+analysis_dir_Parkinson = join(analysis_dir, "Parkinson")
 
-classical_results_dir = "/local/home/lhauptmann/thesis/CL-HAR/signal_processing"
-plot_dir = "/local/home/lhauptmann/thesis/images/thesis"
-results_dir = "/local/home/lhauptmann/thesis/CL-HAR/results"
+data_dir_Parkinson_processed = join(data_dir, "Parkinson_22to7_100Hz")
+data_dir_Parkinson_processed_100Hz_wmetrics = join(data_dir, "Parkinson_22to7_100Hz_wmetrics")
+data_dir_Parkinson_processed_100Hz = join(data_dir, "Parkinson_22to7_100Hz")
+
+data_dir_IEEE = join(data_dir, "IEEE")
+data_dir_IEEE_processed = join(data_dir, "IEEE_processed")
+
+ResNET_oxwearables_weights_path = join(code_dir, "models/mtl_best.mdl")
+
+classical_results_dir = join(code_dir, "signal_processing")
+plot_dir = join(base_dir, "images","thesis")
+results_dir = join(code_dir, "results")
